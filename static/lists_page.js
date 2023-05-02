2e346f64ba806df1f63c8d1d10244816bbcb755d
Maria Klimova <masha-klimova@inbox.ru>
2023-05-02 22:40:25 +0300

    minor design changes

--- static/lists_page.js
+++ static/lists_page.js
@@ -27,9 +27,19 @@ const generateCard = (listProps) => {
             <h5 class="card-title" style="align-self: center;display: contents; ">${listProps.name}</h5>
         </div>
         
-        <p class="card-text">${listProps.description}</p>
+        <p class="card-text" style="font-style: italic">${listProps.description}</p>
         <a href="/word-list?list_id=${listProps.id}" class="btn btn-primary">Просмотреть</a>
         <a href="/learn?list_id=${listProps.id}" class="btn btn-primary">Учить</a>
+    </div>
+    <div style="display: flex; flex-direction: row; align-items: center">
+        <div class="mx-2" style="display: flex; flex-direction: column">
+            <h5>Всего слов</h5>
+            <h2 style="text-align: center">${listProps.words_total}</h2>
+        </div>
+        <div class="mx-2" style="display: flex; flex-direction: column">
+            <h5 class="text-success">Выучено</h5>
+            <h2  class="text-success" style="text-align: center">${listProps.words_learned}</h2>
+        </div>
     </div>
     
   </div>
